f3f15cfc323308ef1a6c5388f242df5f0eac2f16
Brendan Bower <156556347+BBower03@users.noreply.github.com>
2025-03-14 15:38:40 -0400

    Update FireIncidentSubsystem to use udp

--- src/main/java/controllers/FireIncidentSubsystem.java
+++ src/main/java/controllers/FireIncidentSubsystem.java
@@ -1,34 +1,82 @@
 package controllers;
 
 import models.FireEvent;
+
+import javax.xml.crypto.Data;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.*;
 
 /**
  * The FireIncidentSubsystem reads fire event data from a file and sends it to the Scheduler.
  * It also listens for responses from the Scheduler regarding dispatched fire events.
  */
-public class FireIncidentSubsystem implements Runnable {
-    private final Scheduler scheduler;
+public class FireIncidentSubsystem {
+    //private final Scheduler scheduler;
     private final String inputFile;
+
+    DatagramPacket sendPacket, receivePacket;
+    DatagramSocket sendSocket, receieveSocket;
+
+    private final InetAddress serverIP;
+
+    private final int sendPort = 5000;
+    private final int receivePort = 5001;
 
     /**
      * Constructs a FireIncidentSubsystem with a reference to the Scheduler and an input file path
-     * @param scheduler The scheduler that processes fire incidents.
      * @param inputFile The file containing fire event data.
+     * @param serverIP The IP address of the server/scheduler *NOT CURRENTLY IN USE*
      */
-    public FireIncidentSubsystem(Scheduler scheduler, String inputFile) {
-        this.scheduler = scheduler;
+    public FireIncidentSubsystem(String inputFile, InetAddress serverIP) {
         this.inputFile = inputFile;
+        this.serverIP = serverIP;
+        try {
+            sendSocket = new DatagramSocket(sendPort);
+            receieveSocket = new DatagramSocket(receivePort);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void receive() {
+        byte[] data = new byte[100];
+        receivePacket = new DatagramPacket(data, data.length);
+        try {
+            receieveSocket.receive(receivePacket);
+        } catch (IOException e) {
+            System.out.println("receieve error: " + e);
+        }
+
+        System.out.print("Received response: ");
+        int len = receivePacket.getLength();
+        String r = new String(data, 0, len);
+        System.out.print(r);
+    }
+
+    public void send(FireEvent fire) {
+        String message = fire.toString();
+        byte[] msg = message.getBytes();
+        try {
+            sendPacket = new DatagramPacket(msg, msg.length, InetAddress.getLocalHost(), 6001);
+        } catch (UnknownHostException e) {
+            System.out.println("Error: cannot find host: " + e);
+        }
+        System.out.println("Sending fire: " + message);
+        try {
+            sendSocket.send(sendPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Reads fire events from the input file and sends them to the Scheduler for processing.
      * It continuously waits for and handles responses from the Scheduler.
      */
-    @Override
-    public void run() {
+    public FireEvent readFile() {
         // reads file (fire_events.txt)
         try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
             String line;
@@ -44,23 +92,22 @@ public class FireIncidentSubsystem implements Runnable {
 
                 FireEvent event = new FireEvent(time, zoneID, eventType, severity);
 
-                synchronized (System.out) {
-                    System.out.println("[FireIncidentSubsystem] Sent event: " + event);
-                }
-
-                scheduler.receiveFireEvent(event); // send to scheduler
-            }
-
-            while (true) { // wait for responses
-                FireEvent response = scheduler.getResponseForFireIncidentSubsystem();
-                if (response != null) {
-                    synchronized (System.out) {
-                        System.out.println("[FireIncidentSubsystem] Received response: " + response);
-                    }
-                }
+                return event;
             }
         } catch (IOException e) {
             System.err.println("[FireIncidentSubsystem] Error: " + e.getMessage());
         }
+        return null;
+    }
+
+    public static void main(String[] args){
+        try{
+            InetAddress ip = InetAddress.getLocalHost();
+            FireIncidentSubsystem fireSystem = new FireIncidentSubsystem("C:\\Users\\brabo\\Desktop\\FireFightingDroneSystem-main\\src\\main\\resources\\fire_events.txt", ip);
+            FireEvent fire = fireSystem.readFile();
+            fireSystem.send(fire);
+            fireSystem.receive();
+        } catch (UnknownHostException e) {}
+
     }
 }
